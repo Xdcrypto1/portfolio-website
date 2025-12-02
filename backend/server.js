2e90a9cab80d492d42d5c0857738c500c8260a00
Ben Build's <benabor9@gmail.com>
2025-12-02 02:22:26 +0100

    Edited server.js

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get("/resume", (req, res) => {
-    const filePath = path.join(__dirname, "backend/Resume.pdf");
+    const filePath = path.join(__dirname, "Resume.pdf");
     res.download(filePath);
 });
 
